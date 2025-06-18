cc23c2518ccd58a363e2a985cb97501bbe8f1d50
mildrette <mildredfonka1@gmail.com>
2025-06-18 13:40:39 +0100

    new lessons

---
+++ Js-Bascis/script.js
@@ -0,0 +1,14 @@
+// 
+
+//  let pickColors = ['red', 'blue', 'white'];
+//  pickColors[2] = 1;
+// console.log(pickColors.length)
+
+// function
+
+function greet(name) {
+
+    console.log("hello world my name is " + name);
+}
+
+greet("mildred");
