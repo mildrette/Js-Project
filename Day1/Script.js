b3c3ce2df8be930b545f1634c80079f5de92382a
mildrette <mildredfonka1@gmail.com>
2025-07-01 09:23:08 +0100

    learned how to use push and revised js foundamental variable, objects, arrays etc

--- Day1/Script.js
+++ Day1/Script.js
@@ -1,37 +1,28 @@
 let tasks = [];
 
 function addTask(taskName) {
-    const task ={
-        name : taskName,
-        completed: false
-    };
-    tasks.push(task);
-    console.log(`Task added : "${taskName}`);
-}
-
-function listTasks() {
-  console.log("📋 Your Tasks:");
-  tasks.forEach(function(task, index) {
-    const status = task.completed ? "✅" : "❌";
-    console.log(`${index + 1}. ${status} ${task.name}`);
-  });
-}
-
-function completeTask(index) {
-  if (index >= 0 && index < tasks.length) {
-    tasks[index].completed = true;
-    console.log(`✔️ Task "${tasks[index].name}" marked as completed!`);
-  } else {
-    console.log("⚠️ Invalid task number.");
+  const task = {
+    name: taskName,
+    completed: false
   }
+  tasks.push(task)
 }
 
+function handleAddTask(){
+  const input = document.getElementById("taskInput");
+  const taskName = input.value.trim();
 
-function removeTask(index) {
-  if (index >= 0 && index < tasks.length) {
-    const removed = tasks.splice(index, 1);
-    console.log(`🗑️ Task "${removed[0].name}" removed.`);
-  } else {
-    console.log("⚠️ Invalid task number.");
+  if (taskName === "") {
+    alert("Please enter a Task");
+    return;
   }
+
+  addTask(taskName);
+
+  const taskList = document.getElementById("taskList");
+  const li = document.createElement("li");
+  li.textContent = taskName;
+  taskList.appendChild(li);
+
+  input.value = "";
 }
---
+++ Day1/day 1 revisition/script.js
@@ -0,0 +1,33 @@
+// let name = "Zoe";
+// let age = "30";
+// var isFat = false;
+// var student = null;
+// let notSet;
+
+// console.log(notSet)
+// let notSet;
+// const Mildred = {name: "mildred Zoe", age: 30, isFat: "false", student: "null", notSet }
+
+// console.log(Mildred)
+
+// profile files
+const profile = {
+    fullName: "mildred Fonka", 
+    age: 30,
+    country: "cameroonain",
+    isStudent: false,
+    hobbies: ["coding", "singing", "eating", "sleeping", "learning New Things"]
+}
+
+profile.hobbies.push("prayers", "writing", "reading")
+
+console.log("your name is : ", profile.fullName),
+console.log("your Hobbies are: ", profile.hobbies)
+
+// array of friends
+
+const friends = ['vanessa', "light", "favour", "sasha", "joy", "wisdom","lum"];
+friends.push("petra", "blessings", "queenta")
+console.log(friends)
+console.log("my Best Friends name is : ", friends[5]);
+console.log("Total Friends is : ", friends.length)
