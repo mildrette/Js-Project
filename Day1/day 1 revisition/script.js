69724cae69314ccde87650505b186aa9552ed527
mildrette <mildredfonka1@gmail.com>
2025-07-01 10:10:24 +0100

    learned how to use loops

--- Day1/day 1 revisition/script.js
+++ Day1/day 1 revisition/script.js
@@ -10,24 +10,34 @@
 
 // console.log(Mildred)
 
-// profile files
-const profile = {
-    fullName: "mildred Fonka", 
-    age: 30,
-    country: "cameroonain",
-    isStudent: false,
-    hobbies: ["coding", "singing", "eating", "sleeping", "learning New Things"]
-}
+// // profile files
+// const profile = {
+//     fullName: "mildred Fonka", 
+//     age: 30,
+//     country: "cameroonain",
+//     isStudent: false,
+//     hobbies: ["coding", "singing", "eating", "sleeping", "learning New Things"]
+// }
 
-profile.hobbies.push("prayers", "writing", "reading")
+// profile.hobbies.push("prayers", "writing", "reading")
 
-console.log("your name is : ", profile.fullName),
-console.log("your Hobbies are: ", profile.hobbies)
+// console.log("your name is : ", profile.fullName),
+// console.log("your Hobbies are: ", profile.hobbies)
 
-// array of friends
+// // array of friends
 
-const friends = ['vanessa', "light", "favour", "sasha", "joy", "wisdom","lum"];
-friends.push("petra", "blessings", "queenta")
-console.log(friends)
-console.log("my Best Friends name is : ", friends[5]);
-console.log("Total Friends is : ", friends.length)
+// const friends = ['vanessa', "light", "favour", "sasha", "joy", "wisdom","lum"];
+// friends.push("petra", "blessings", "queenta")
+// console.log(friends)
+// console.log("my Best Friends name is : ", friends[5]);
+// console.log("Total Friends is : ", friends.length)
+
+/* learning Loops, here learning the For Loops */
+const loops = ["money", "food", "fruits", "hobbies", "coding", "dresses", "reading", "writing", "cleaning"];
+
+for (let i = 0; i < loops.length; i++){
+    console.log(`${i + 1}. ${loops[1]}`)
+};
+
+
+
