71b7c1563bc4dedb983425861c96f958e3b2b204
mildrette <mildredfonka1@gmail.com>
2025-07-04 21:38:03 +0100

    Dom

--- Day 4/script.js
+++ Day 4/script.js
@@ -1,1 +1,13 @@
 //  DOM
+
+const moodText = document.getElementById("moodText");
+const button = document.getElementById("moodbtn");
+
+const moods = ["😊 Happy", "😢 Sad", "😠 Angry", "😴 Sleepy"];
+
+
+function changeMood() {
+    const randomIndex = Math.floor(Math.random() * moods.length);
+moodText.innerText = moods[randomIndex]};
+
+button.addEventListener("click", changeMood);
